fe19071c7ef1f233a52a3ed0dd0ba426d334465e
J35P312 <jesper.eisfeldt@scilifelab.se>
2016-09-17 18:43:22 +0200

    modified:   FindSV.py

--- FindSV.py
+++ FindSV.py
@@ -57,6 +57,10 @@ def retrieve_status(status,nextflow_output):
                 tmp_dict[prefix][content[0]]["status"]="FAILED:{}".format(process)
                 tmp_dict[prefix][content[0]]["message"] += "\n" + line.strip()
                 tmp_dict[prefix]["status"]="FAILED:{}".format(process)
+            else:
+                print "FAILURE"
+                print message.strip()
+                break
             
     for sample in tmp_dict:
             	        	
